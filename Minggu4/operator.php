d86ef8aae0d3dca2a77bed969480679201423a30
luthfi27upi <galut012@gmail.com>
2024-09-20 09:06:42 +0700

    Jobsheet 4

---
+++ Minggu4/operator.php
@@ -0,0 +1,26 @@
+<?php
+$a=10;
+$b= 5;
+
+$hasilTambah= $a+$b;
+$hasilKurang= $a-$b;
+$hasilKali= $a*$b;
+$hasilBagi= $a/$b;
+$sisaBagi= $a%$b;
+$pangkat= $a**$b;
+
+echo "Hasil Penambahan : $hasilTambah <br>";
+echo "Hasil Pengurangan : $hasilKurang <br>";
+echo "Hasil Perkalian : $hasilKali <br>";
+echo "Hasil Pembagian : $hasilBagi <br>";
+echo "Sisa Bagi : $sisaBagi <br>";
+echo "Hasil Perpangkatan : $pangkat <br>";
+
+$hasilAnd=$a&&$b;
+$hasilOr=$a||$b;
+$hasilNotA=!$a;
+$hasilNotB=!$b;
+
+echo "Hasil And : $hasilAnd <br>";
+echo "hasil OR : ";
+?>
