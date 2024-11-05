37339884e192ee09d05d0338de09ddb5919be154
luthfi27upi <galut012@gmail.com>
2024-11-05 22:47:47 +0700

    Jobsheet 10

--- Minggu10/Koneksi.php
+++ Minggu10/Koneksi.php
@@ -2,5 +2,5 @@
 $koneksi = mysqli_connect("localhost","root","","webjs10");
 
 if(mysqli_connect_errno()){
-    die("Koneksi database gagal : ".mysqli_connect_error());
+    die("Koneksi database gagal : " .mysqli_connect_error());
 }
