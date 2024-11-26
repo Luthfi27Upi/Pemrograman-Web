18a3f603d7125b5c67f148afcc3a82a0586da6cb
luthfi27upi <galut012@gmail.com>
2024-11-26 18:46:52 +0700

    Jobsheet 13

---
+++ Minggu13/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost"; // Nama host
+$username = "root"; // Nama user
+$password = ""; // Password, sesuaikan jika ada
+$dbname = "webjs10"; // Nama database
+
+$koneksi = mysqli_connect($host, $username, $password, $dbname);
+
+if ($koneksi) {
+    echo "Koneksi berhasil. <br />";
+} else {
+    echo "Koneksi Gagal.";
+    die("Error: " . mysqli_connect_error());
+}
