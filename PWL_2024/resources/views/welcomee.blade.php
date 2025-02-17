0c3176cf63ace977637ddf8679eef51d4e2603cc
luthfi27upi <galut012@gmail.com>
2025-02-17 08:09:31 +0700

    test commit

---
+++ PWL_2024/resources/views/welcomee.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PWL_2024</title>
+</head>
+<body>
+    <h2>Pemrograman Web Lanjut</h2>
+    <h3>Semangat Belajar Laravel 10</h3>
+</body>
+</html>
